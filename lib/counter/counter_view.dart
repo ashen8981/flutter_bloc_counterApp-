b7c00a10c0ae58d8f3a3aac6dadaa864d9acae63
Ashen Malindu <ashenmalindu112@gmail.com>
2025-02-17 17:17:07 +0530

    UI

---
+++ lib/counter/counter_view.dart
@@ -0,0 +1,13 @@
+//counter view: responsible for UI
+//use blockBuilder
+import 'package:flutter/material.dart';
+
+class CounterView extends StatelessWidget {
+  const CounterView({super.key});
+
+  //build UI
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
